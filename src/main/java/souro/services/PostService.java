e0b8560ef96b213e669017f5c25e846560fa3da8
souro89 <souro89@gmail.com>
2019-03-29 11:35:36 +0400

    Technical Blog Application - JDBC Connection

--- src/main/java/souro/services/PostService.java
+++ src/main/java/souro/services/PostService.java
@@ -3,6 +3,8 @@ package souro.services;
 import org.springframework.stereotype.Service;
 import souro.models.Post;
 
+import javax.xml.transform.Result;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -13,24 +15,40 @@ public class PostService {
 
         ArrayList<souro.models.Post> posts = new ArrayList<>();
 
-        Post post1 = new souro.models.Post();
-        post1.setTitle("Post 1");
-        post1.setBody("Post Body 1");
-        post1.setDate(new Date());
+//        Post post1 = new souro.models.Post();
+//        post1.setTitle("Post 1");
+//        post1.setBody("Post Body 1");
+//        post1.setDate(new Date());
+//
+//        Post post2 = new souro.models.Post();
+//        post2.setTitle("Post 2");
+//        post2.setBody("Post Body 2");
+//        post2.setDate(new Date());
+//
+//        Post post3 = new souro.models.Post();
+//        post3.setTitle("Post 3");
+//        post3.setBody("Post Body 3");
+//        post3.setDate(new Date());
+//
+//        posts.add(post1);
+//        posts.add(post2);
+//        posts.add(post3);
 
-        Post post2 = new souro.models.Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post Body 2");
-        post2.setDate(new Date());
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/TechnicalBlog","postgres","admin");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select * from posts");
+            while(rs.next()){
+                Post ps = new Post();
+                ps.setTitle(rs.getString("title"));
+                ps.setBody(rs.getString("body"));
+                posts.add(ps);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
 
-        Post post3 = new souro.models.Post();
-        post3.setTitle("Post 3");
-        post3.setBody("Post Body 3");
-        post3.setDate(new Date());
-
-        posts.add(post1);
-        posts.add(post2);
-        posts.add(post3);
 
         return posts;
     }
@@ -39,12 +57,30 @@ public class PostService {
 
         ArrayList<Post> posts = new ArrayList<>();
 
-        Post post1 = new souro.models.Post();
-        post1.setTitle("First Post");
-        post1.setBody("This is your post");
-        post1.setDate(new Date());
+//        Post post1 = new souro.models.Post();
+//        post1.setTitle("First Post");
+//        post1.setBody("This is your post");
+//        post1.setDate(new Date());
+//
+//        posts.add(post1);
 
-        posts.add(post1);
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/TechnicalBlog","postgres","admin");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select * from posts where id=4");
+            while(rs.next()){
+                Post ps = new Post();
+                ps.setTitle(rs.getString("title"));
+                ps.setBody(rs.getString("body"));
+                posts.add(ps);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
         return posts;
 
 
